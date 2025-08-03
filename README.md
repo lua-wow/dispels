093201099c0127b541cf7b5a38e0382cc9358f91
pedrozc90 <pedrozc90@gmail.com>
2025-08-03 11:47:32 -0300

    patch 5.5.0

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Dispels
 
 World of Warcraft addon to announce when a debuff is dispelled or buff is stole.
+
+## License
+
+Please, read [LICENSE](./LICENSE) file.
--- init.lua
+++ init.lua
@@ -3,11 +3,12 @@ local addon, ns = ...
 local Dispels = CreateFrame("Frame")
 Dispels.debuffs = {}
 
--- https://wowpedia.fandom.com/wiki/WOW_PROJECT_ID
+-- https://warcraft.wiki.gg/wiki/WOW_PROJECT_ID
 Dispels.isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
 Dispels.isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
 Dispels.isTBC = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
 Dispels.isWrath = (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC)
 Dispels.isCata = (WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC)
+Dispels.isMoP = (WOW_PROJECT_ID == WOW_PROJECT_MISTS_CLASSIC)
 
 ns.Dispels = Dispels
